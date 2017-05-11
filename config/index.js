29a09759d12c5e64768a863612200d33060072a5
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-05-11 15:40:40 +0200

    Updates Node. Changes alias

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   SAML_YAR_SECRET: process.env.SAML_YAR_SECRET || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
   SAML_ENCRYPTOR_SECRET: process.env.SAML_ENCRYPTOR_SECRET || 'Louie Louie, oh no, I got to go Louie Louie, oh no, I got to go',
   SAML_JWT_SECRET: process.env.SAML_JWT_SECRET || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
-  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.micro.t-fk.no',
+  SESSION_STORAGE_URL: process.env.SESSION_STORAGE_URL || 'https://tmp.storage.tjeneste.win',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'micro-saml-mock',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
